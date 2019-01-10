610363c245ce8ba81892f68febc4efee3df07b0b
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2019-01-10 16:41:51 -0500

    uncomment printing function

--- modules/printer.py
+++ modules/printer.py
@@ -8,4 +8,4 @@ def write_string_to_printer(string):
     fp.write(string.encode('latin1'))
     fp.close()
     subprocess.call(["cat", fp.name])
-    # subprocess.call([tfunilx, "SendFileCmd", fp.name]) # comment to disable printing
+    subprocess.call([tfunilx, "SendFileCmd", fp.name]) # comment to disable printing
