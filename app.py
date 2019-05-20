db97ed3364c55567bfba707fed03369ddd1a6d6f
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2019-05-20 15:52:01 -0500

    error handlers (production), clean template

--- app.py
+++ app.py
@@ -219,8 +219,11 @@ def reporteZ():
     return redirect(url_for('index'))
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+def page_not_found(error):
+    return render_template('404.html', error = error)
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('404.html', error = error)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
