528ac0a13ef51f0771138acf519bee4c6b8e8a6e
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2019-06-06 16:24:59 -0500

    debug mode

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ def internal_error(error):
     return render_template('404.html', error = error)
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
