fac12a5909e605427c220cf065e68628f2675b11
carlos piad <carlospiad@gmail.com>
2021-09-02 21:02:58 -0500

    Prueba

--- modules/printer.py
+++ modules/printer.py
@@ -7,7 +7,7 @@ tfunilx = './tfunilx'
 
 def write_string_to_printer(string):
     fp = tempfile.NamedTemporaryFile(delete=False)
-    fp.write(string.encode('latin1'))
+    fp.write(string.encode('utf-8'))
     fp.close()
     subprocess.call(["cat", fp.name])
     # comment to disable printing
